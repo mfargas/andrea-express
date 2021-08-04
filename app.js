a8dc7764a30b31829b2c8533a42c8900544079da
Marjoree <65003463+mfargas@users.noreply.github.com>
2021-08-03 17:01:09 -0700

    Update issues with routes

--- app.js
+++ app.js
@@ -1,13 +1,14 @@
 var createError = require('http-errors');
-var express = require('express');
-var path = require('path');
+const express = require('express');
+const path = require('path');
+const router = express.Router();
+const app = express();
+
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
-
-var app = express();
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
