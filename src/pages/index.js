fbcaef8425efb2125080e4f2d3b0176a3a70788e
Marjoree <65003463+mfargas@users.noreply.github.com>
2021-09-22 10:21:31 -0700

    Restructure Andrea's portfolio in Gatsby

---
+++ src/pages/index.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import Layout from '../components/layout'
+
+export default function Home() {
+    return( 
+        <Layout pageTitle='Andrea Home'>
+            <h1>Hello Gatsby!</h1>
+        </Layout>
+        
+    )
+}
