6ef19b9f80989af663a56412d64bedf4d0153a07
Marjoree <65003463+mfargas@users.noreply.github.com>
2022-03-20 11:43:51 -0700

    Debug query error

--- src/components/HomeIntro.js
+++ src/components/HomeIntro.js
@@ -1,7 +1,6 @@
 import * as React from 'react'
 import * as styles from '../stylesheets/homeIntro.module.css'
-// import { StaticImage } from "gatsby-plugin-image"
-import Profile from './Profile';
+import { StaticImage } from "gatsby-plugin-image"
 import { RiAmazonFill } from "react-icons/ri";
 
 const HomeIntro = () => {
@@ -11,8 +10,9 @@ const HomeIntro = () => {
                 <h1>Actively helping our communities</h1>
             </div>
             <div className={styles.bio}>
-                <h2>Legal Attorney Assistant For Amazon <RiAmazonFill /></h2>
-                <Profile />
+                <h2>legal attorney's assistant</h2>
+                <StaticImage src='../assets/images/andrea-congress.png' alt="andrea at the capitol"/>
+                <RiAmazonFill />
             </div>
         </div>
     )
--- src/components/Layout.js
+++ src/components/Layout.js
@@ -1,15 +1,26 @@
 import * as React from 'react'
 import Header from './Header'
 import Footer from './Footer'
+import { StaticQuery, graphql, useStaticQuery } from 'gatsby'
 import * as styles from "../stylesheets/layout.module.css"
 
 const Layout = ({ pageTitle, children }) => {
+    const data = useStaticQuery(graphql`
+        query MyQuery{
+            site{
+                siteMetadata{
+                    title
+                }
+            }
+        }
+    `)
+
     return (
         <div className={styles.container}>
-            <title className={styles.heading}>{pageTitle}</title>
+            <title className={styles.heading}>{pageTitle} | {data.site.siteMetadata.title}</title>
             <Header />
             <main>
-                <h1 className={styles.heading}>{pageTitle}</h1>
+                <h1 className={styles.heading}>{data.siteMetadata.title}</h1>
                 <article>
                     {children}
                 </article>
