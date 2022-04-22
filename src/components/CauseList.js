d03c1b5b9136890e3cb06f9a34b1065092b6169d
Marjoree <65003463+mfargas@users.noreply.github.com>
2022-04-22 11:23:00 -0700

    Update config and causes page

--- src/components/CauseList.js
+++ src/components/CauseList.js
@@ -11,6 +11,11 @@ const CauseList = () => {
                 <Link to='https://www.immdef.org/lbjf' target='_blank' className={styles.causeLink}>Go to Organization</Link>
                 <p>Long Beach initiative that provides legal representation for immigrants who live or work in Long Beach and are facing deportation.</p>
             </div>
+            <div className={styles.cause}>
+                <h3>Ukraine Humanitarian Fund</h3>
+                <Link to='https://crisisrelief.un.org/ukraine-crisis' target='_blank' className={styles.causeLink}>Go to Organization</Link>
+                <p>United Nations initiative delivering assistance and protection to the people of Ukraine.</p>
+            </div>
         </section>
     )
 }
